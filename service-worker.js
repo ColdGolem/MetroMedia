cb5f02a515dc1b020adbaa027fb1377707b070e1
ColdGolem <deeperseekerz@gmail.com>
2026-03-13 00:28:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = "metropulse-cache-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/demo.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
+// Install event: cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event: serve cached files if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
