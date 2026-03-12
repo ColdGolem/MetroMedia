592ac64269f83b8beed66e838401105c6627654d
ColdGolem <deeperseekerz@gmail.com>
2026-03-13 00:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+function report(level){
 
+console.log("Crowd level:",level);
+
+document.getElementById("unlockBox").classList.remove("hidden");
+
+}
+
+function watchAd(){
+
+alert("Ad plays here to unlock chat preview");
+
+document.getElementById("chatArea").classList.remove("hidden");
+
+startTimer();
+
+}
+
+function startTimer(){
+
+let time=600;
+
+let timer=setInterval(()=>{
+
+time--;
+
+let m=Math.floor(time/60);
+let s=time%60;
+
+document.getElementById("timer").innerText=
+"Chat unlocked for "+m+":"+s;
+
+if(time<=0){
+
+clearInterval(timer);
+
+alert("Session ended. Watch another ad or pay.");
+
+}
+
+},1000);
+
+}
+
+function payChat(){
+
+/*
+
+RAZORPAY PAYMENT (ENABLE AFTER API KEYS)
+
+var options={
+key:"YOUR_KEY",
+amount:"1000",
+currency:"INR",
+name:"MetroPulse",
+description:"Unlock Chat",
+handler:function(response){
+alert("Chat unlocked!");
+}
+};
+
+var rzp=new Razorpay(options);
+rzp.open();
+
+*/
+
+alert("Payment system coming soon");
+
+}
